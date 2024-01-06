eeee599fc16271913a329931393d2ff1a448b963
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2024-01-06 17:22:20 +0200

    edit arg names

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 # Create command line arguments
 argParser = argparse.ArgumentParser()
 argParser.add_argument(
-    "-i",
-    "--in",
+    "-p",
+    "--playlist_name",
     help="The name of the playlist that contains the wanted content.",
     required=True,
 )
 argParser.add_argument(
-    "-o", "--out", help="The name of the output playlist", required=True
+    "-o", "--out_playlist", help="The name of the output playlist", required=True
 )
 args = argParser.parse_args()
 
@@ -23,6 +23,9 @@ load_dotenv()
 scope = "user-library-read"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_playlists()
-for item in results["items"]:
-    print(item["name"])
+playlists = sp.current_user_playlists()
+for item in playlists["items"]:
+    if item["name"] == args.playlist_name:
+        print(True)
+    else:
+        print(False)
