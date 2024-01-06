14f5aa649db4472ccb5a6796f2818efe3adeb7c2
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2024-01-06 17:16:35 +0200

    add argument parsing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
+import argparse
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
+# Create command line arguments
+argParser = argparse.ArgumentParser()
+argParser.add_argument(
+    "-i",
+    "--in",
+    help="The name of the playlist that contains the wanted content.",
+    required=True,
+)
+argParser.add_argument(
+    "-o", "--out", help="The name of the output playlist", required=True
+)
+args = argParser.parse_args()
+
+# Load spotify credentials
 load_dotenv()
 
 scope = "user-library-read"
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+results = sp.current_user_playlists()
+for item in results["items"]:
+    print(item["name"])
