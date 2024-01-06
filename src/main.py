e703e3219034a4026bc0f1a2982458680d1c9328
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2024-01-06 16:55:58 +0200

    getting started

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
